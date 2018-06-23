c7d20ac7012496e1f71fa8c3da1ff3ba2036cd0f
Andrew Sosa <andrewsosa001@gmail.com>
2018-06-23 03:58:19 -0400

    API can prevent duplicates

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // const mongodb = require('mongodb').MongoClient;
 const monk = require('monk');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const serverless = require('serverless-http');
 const express = require('express');
@@ -7,17 +9,28 @@ const express = require('express');
 const app = express();
 const db = monk(process.env.DB_URI);
 
+// Middleware
+app.use(bodyParser.json({ strict: false }));
+app.use(morgan('dev'));
 
+// Routes
 app.get('/', (req, res) => res.send('Hello, world!'));
-
 app.get('/deaths', (req, res) => {
   db.get('deaths').find({}).then(docs => res.json(docs));
 });
-
 app.post('/deaths', (req, res) => {
-  res.sendStatus(400);
+  const deaths = db.get('deaths');
+  deaths.createIndex('name', { unique: true });
+  console.log(req.body)
+  deaths.insert([req.body])
+    .then((docs) => {
+      console.log(`Inserted ${docs}`);
+      res.send(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
-
-// app.listen(3000, () => console.log('uri:', process.env.DB_URI));
 module.exports.handler = serverless(app);
